b2cda539c5f157600a4a87599208bba52fc67cd8
Rio Cantre <smittenaf301icu@gmail.com>
2021-06-24 17:08:31 +0200

    Updated html, css and js

--- js/app.js
+++ js/app.js
@@ -11,4 +11,36 @@ function buildNav() {
         liList.innerHTML = `<span> ${sectionHeader}</span>`;
     }
 };
-document.addEventListener('DOMContentLoaded', buildNav);
+buildNav();
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+myFunction();
+
+var coll = document.getElementsByClassName(".collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
