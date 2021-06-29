38e27ee097553a8e27e7eb30b86a7aa72586b994
Rio Cantre <smittenaf301icu@gmail.com>
2021-06-29 17:03:30 +0200

    Updated html, css and js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,44 @@
-let docElement = document.documentElement;
-let navList = document.getElementById('navBar');
-let fragment = document.createDocumentFragment();
-let navSection = document.querySelectorAll('section');
-let backToTop = document.getElementById('goTop');
+var docElement = document.documentElement;
+var navList = document.getElementById('navBar');
+var fragment = document.createDocumentFragment();
+var navSection = document.querySelectorAll('section');
+var backToTop = document.getElementById('goTop');
 console.log(navSection);
 console.log(backToTop);
 
 
-function isInViewport (elem) {
-    const bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
+// build the nav
 function buildNav() {
-  navSection.forEach(function (navSection) {
-    var li = document.createElement('li');
-    li.textContent = navSection.getAttribute('data-nav');
-    fragment.appendChild(li);
-    console.log(li);
-  });
-  
-  navList.appendChild(fragment);
+    navSection.forEach(function (navSection) {
+        var li = document.createElement('li');
+        li.textContent = navSection.getAttribute('data-nav');
+        fragment.appendChild(li);
+        console.log(li);
+    });
+    
+    navList.appendChild(fragment);
 }
-buildNav();
 
 
-function setActiveNav() {
-  for (let i = 0; i < navSection.length; i++){
-    if (isInViewport(navSection[i])) {
-      navSection[i].classList.add('active-class');
+// build a button to scroll the page back to Top
+window.addEventListener('scroll', function () {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        backToTop.classList.add('myBtn');
     } else {
-      navSection[i].classList.remove('active-class');
+        backToTop.classList.remove('myBtn');
+        
     }
-    // console.log(navSection[i]);
-  }
-}
-document.addEventListener('scroll', function () {
-  setActiveNav();
 })
+function goToTop() {
+    document.getElementById('goTop').addEventListener('click', function () {
+        window.scrollTo({
+            left: 0,
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
 
 // sticky nav
 window.onscroll = function() {myFunction()};
@@ -51,31 +47,12 @@ var navbar = document.getElementById("navBar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
-myFunction();
-
-let activeScrolling = () => {
-  let viewportY = docElement.clientHeight - 152;
-  if (docElement.scrollTop >= viewportY) {
-    backToTop.classList.add("myBtn");
-  } else {
-    backToTop.classList.remove("myBtn");
-  }
-}
-
-let scrollPageToTop = () => {
-  docElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
-
-backToTop.addEventListener("click", scrollPageToTop);
 
 
 // collapsible content
@@ -83,13 +60,18 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
 }
+
+
+buildNav();
+goToTop();
+myFunction();
